c79cf1f11605a8fa3367f6c472eab67102b3069c
parkerwy <parkerwy@gmail.com>
2017-01-06 16:32:14 +0800

    adjust page layout

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 480
     })
 
     // and load the index.html of the app.
