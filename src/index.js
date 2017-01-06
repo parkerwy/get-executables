026acc069fcc54988a75b157c35c1080b60f1aee
parkerwy <parkerwy@gmail.com>
2017-01-06 12:31:24 +0800

    mvp milestone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('jquery');
 const os = require('os');
+const fs = require('fs');
+const request = require('request');
 
 function download() {
     console.log('download called.');
@@ -9,7 +11,11 @@ function download() {
     var port = $('#proxyPort').val();
     var user = $('#proxyUser').val();
     var password = $('#proxyPassword').val();
-    var proxy = `http://${user}:${password}@${host}:${port}`;
+    if (host && port && user && password) {
+        var proxy = `http://${user}:${password}@${host}:${port}`;
+    } else {
+        var proxy = "";
+    }
     console.log(proxy);
     requestWithProxy(url, proxy);
 }
@@ -17,5 +23,17 @@ function download() {
 function requestWithProxy(url, proxy) {
     var filename = url.split('/').pop().split('#')[0].split('?')[0];
     var saveAs = `${os.homedir()}/Desktop/${filename}`;
-    console.log(saveAs);
+    console.log(`download [${url}] as [${saveAs}] via proxy [${proxy}]`);
+    var r = request.defaults({
+        'proxy': proxy
+    });
+    r.get(url)
+        .on('response', function(response) {
+            var status = response.statusCode;
+            var type = response.headers['content-type'];
+            var length = response.headers['content-length'];
+            console.log(`Status [${status}], Content Type [${type}], Content Length [${length}]`);
+        })
+        .pipe(fs.createWriteStream(saveAs));
+    console.log("download completed.");
 }
